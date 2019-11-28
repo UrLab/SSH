e0f4f5492eb4c720db2feda00e73438266d2802d
fgrimau <florent.grimau@ulb.be>
2019-11-28 17:48:25 +0100

    Commented the code for more readability

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from database import DataBase
 from pygame.locals import (
     QUIT,
     K_RETURN,
@@ -16,26 +15,34 @@ from constants import (
     SCREEN_X,
     SCREEN_Y
 )
+from database import DataBase
 
 
-# Get the scan result
+# Get the cleaned scan result
 def fetchScan(event, text):
-    if event.type == KEYDOWN:
+    if event.type == KEYDOWN:  # Get every input from the scan
+        # The "return" key is the final input from the scan
         if event.key == K_RETURN:
             return text, True
         elif event.key == K_BACKSPACE:
             text = text[:-1]
         else:
+            # event.scancode automatically converts the result as string
             text += str((event.scancode-9) % 10)
     return text, False
 
 
 def main():
+    # Handles the data from a json file, needs to be upgraded
     db = DataBase('data/db.json')
+
+    # The result is a string containing the scan value and a
+    # boolean indicating if its the complete scan or not
     scan = "", False
-    running = True
+    running = True  # Defines wether the application is running or not
     title = fonts["25"].render(
         "Scannez un produit pour en connaitre sa valeur", 1, (0, 0, 0))
+    # The current product (Name, Price, Image) as images
     current_product = [None, None, None]
 
     while running:
@@ -49,6 +56,7 @@ def main():
             (0.9*SCREEN_X, urlabBanner.get_size()[1]+title.get_size()[1]+10)
         )
 
+        # If a product has been scanned
         if current_product[2] is not None:
             screen.blit(
                 current_product[2], (
@@ -81,13 +89,18 @@ def main():
                 if event.key == K_ESCAPE:
                     running = False
             scan = fetchScan(event, scan[0])
+            # If a scan has been completed
             if scan[1]:
                 current_product = db.fetch(scan[0])
+                # If no product was found with the scan
                 if current_product[0] == "":
                     current_product[0] = "Not found !"
                     current_product[1] = "99999999999"
+                # If there's no image associated to the product
                 if current_product[2] == "":
                     current_product[2] = "imgs/products/Not_Found.png"
+                # Refactor current product to only have images
+                # that can be printed to the screen
                 current_product = [
                     fonts["25"].render(
                         current_product[0],
