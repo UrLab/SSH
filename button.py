bc3221fd3281d8f5e7d9ab74f74a810460bee828
fgrimau <florent.grimau@ulb.be>
2019-12-02 17:54:15 +0100

    Added the ability to change the color of a button

--- button.py
+++ button.py
@@ -8,8 +8,15 @@ class Button(object):
     def __init__(self, pos, size, text, font):
         self.pos = pos
         self.size = size
+
+        self.fg = (0, 0, 0)
+        self.bg = (0, 0, 0)
+        self.contour = (50, 50, 50)
+
+        self.font = font
+        self.text = text
         self.rect = pygame.Rect(pos, size)
-        self.image = font.render(text, 1, (0, 0, 0))
+        self.image = font.render(text, 1, self.fg)
         self.text_pos = (
             self.pos[0] + self.size[0]//2 - self.image.get_size()[0]//2,
             self.pos[1] + self.size[1]//2 - self.image.get_size()[1]//2
@@ -18,9 +25,16 @@ class Button(object):
     def collide(self, pos):
         return self.rect.collidepoint(pos[0], pos[1])
 
+    def set_color(self, fg=None, bg=None, contour=None):
+        self.fg = fg if fg is not None else self.fg
+        self.bg = bg if bg is not None else self.bg
+        self.contour = contour if contour is not None else self.contour
+
+        self.image = self.font.render(self.text, 1, self.fg)
+
     def draw(self, screen, offset):
         pygame.draw.rect(
-            screen, (50, 50, 50),
+            screen, self.contour,
             self.rect.move(offset[0], offset[1]), 2
         )
         screen.blit(
